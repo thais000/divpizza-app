239e16449a7613d839a0b66480f90f8b40d17316
THAIS REIS CESARIO <591831508@alunos.etec.itaqua>
2019-08-05 22:31:21 -0300

    criação de paginas de adição de pizza e laço de repetição

---
+++ src/app/add-pizza/add-pizza.page.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-add-pizza',
+  templateUrl: './add-pizza.page.html',
+  styleUrls: ['./add-pizza.page.scss'],
+})
+export class AddPizzaPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  salvar(pizza) {
+    console.log("hdsuiahdiusa");
+    
+    const pizzaDados = pizza.value
+    console.log(pizzaDados)
+
+  }
+}
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  catalogo: Array<Object> = []
 
-  constructor() {}
-
+  ionViewDidEnter() {
+    console.log("EXECULTOU O VIEW DID ENTER")
+    this.catalogo.push({
+      nome: 'queojo maluco gourmet',
+      descricao: 'Queijo, catupiry, batata palha, cogumelos, manjerição e ,mais umacolher de caviar',
+      preco: 'R$72,00',
+    })
+  }
 }
